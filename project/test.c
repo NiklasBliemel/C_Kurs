e4e6ba6748d2d38c0d4f5bcc0dbb6b9b87b9940e
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2025-04-18 15:36:18 +0200

    Base Tensor Functions

--- project/test.c
+++ project/test.c
@@ -1,2 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include "matrix.h"
+
+int main()
+{
+    unsigned int shape[] = {4, 2, 2, 2, 2};
+    Tensor *tensor = zeros(shape);
+    print_shape(tensor);
+    print_tensor(tensor);
+    pop(tensor);
+}
