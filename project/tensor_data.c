6aa05f72865ebdd56db2f82ecdc95ff1c466bc3d
Niklas Bliemel <niklas.bliemel@stud.uni-regensburg.de>
2025-04-25 18:07:18 +0200

    fin fin

--- project/tensor_data.c
+++ project/tensor_data.c
@@ -10,6 +10,7 @@ void save_tensor(Tensor *t, char *file_name)
         perror("Failed to open file");
         return;
     }
+    // write every part of the Tensor structure one by one in binary (order is important for loading tensor)
     fwrite(&t->num_entries, sizeof(unsigned), 1, file);
     fwrite(t->data, sizeof(double), t->num_entries, file);
     fwrite(&t->rank, sizeof(int), 1, file);
@@ -26,10 +27,14 @@ void load_tensor(Tensor *t, char *file_name)
         perror("Failed to open file");
         return;
     }
+    // first read extracts num entries
     fread(&t->num_entries, sizeof(unsigned), 1, file);
+    // use numentries to relocate enough memory and know how long to read data
     t->data = realloc(t->data, t->num_entries * sizeof(double));
     fread(t->data, sizeof(double), t->num_entries, file);
+    // read rank, which is always stored after data
     fread(&t->rank, sizeof(int), 1, file);
+    // relocate memory for shape and stride and continue reading (reading - length also defined by saved rank)
     t->shape = realloc(t->shape, t->rank * sizeof(unsigned));
     t->stride = realloc(t->stride, t->rank * sizeof(unsigned));
     fread(t->shape, sizeof(unsigned), t->rank, file);
